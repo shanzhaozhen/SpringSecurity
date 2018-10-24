b1d1139d3f2d19157e3219e6ba73645fa92df7c2
shanzhaozhen <204447118@qq.com>
2018-10-24 10:10:42 +0800

    修复了更新实体不会更新时间

--- src/main/java/org/shanzhaozhen/springsecurity/SpringsecurityApplication.java
+++ src/main/java/org/shanzhaozhen/springsecurity/SpringsecurityApplication.java
@@ -2,7 +2,9 @@ package org.shanzhaozhen.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing              //解决jpa使用CreatedDate不能生成时间
 @SpringBootApplication
 public class SpringsecurityApplication {
 
